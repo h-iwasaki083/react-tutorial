9299f01af260390516972709d8c2f693ca780520
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2023-06-06 20:38:00 +0900

    コンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 // コンポーネントを作成するファイル
 import React from 'react';
+
 // export:Square関数を、ファイルの外部からアクセス出来るように設定。
+// コンポーネントアーキテクチャ：再利用可能なコンポーネントの作成
+function Square({ value }){
+  return <button className="squre">{ value }</button>;
+}
+
+// JSX構文を利用してSquareコンポーネントをrenderする
 export default function Board() {
   return (
     <div>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </div>
   );
